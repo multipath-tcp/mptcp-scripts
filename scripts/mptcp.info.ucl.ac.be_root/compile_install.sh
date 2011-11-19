b7c32f2dc79d5ee78039f23b53cc8058e9cc9ce0
Christoph Paasch <christoph.paasch@uclouvain.be>
2011-11-20 00:10:26 +0200

    More fix to compile_install.sh
    Signed-off-by: Christoph Paasch <christoph.paasch@uclouvain.be>

--- scripts/mptcp.info.ucl.ac.be_root/compile_install.sh
+++ scripts/mptcp.info.ucl.ac.be_root/compile_install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+
 # Create and update the debian-repos
 file=`basename $0` 
 trap "mutt -s \"$file crontab-failure\" -- christoph.paasch@uclouvain.be < /tmp/${file}.log; exit 1" ERR
