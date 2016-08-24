a83c92ded017a706f73865ca41f1623e18f2f99d
Christoph Paasch <cpaasch@apple.com>
2016-08-24 05:52:41 +0000

    kernel

--- scripts/debian_build/create_kernel.sh
+++ scripts/debian_build/create_kernel.sh
@@ -10,6 +10,8 @@ DATE=`date "+%Y%m%d%H%M%S"`
 KVERS=`make kernelversion`
 make -j 8 deb-pkg DEBEMAIL='christoph.paasch@gmail.com' DEBFULLNAME='Christoph Paasch' LOCALVERSION=.mptcp KDEB_PKGVERSION=${DATE}
 
+cd $HOME
+
 # Create meta-package
 rm -Rf linux-mptcp
 
@@ -24,7 +26,7 @@ echo "Version: ${DATE}" >> $ctrl
 echo "Section: main" >> $ctrl
 echo "Priority: optional" >> $ctrl
 echo "Architecture: all" >> $ctrl
-echo "Depends: linux-headers-${KVERS}, linux-image-${KVERS}" >> $ctrl
+echo "Depends: linux-headers-${KVERS}.mptcp, linux-image-${KVERS}.mptcp" >> $ctrl
 echo "Installed-Size:" >> $ctrl
 echo "Maintainer: Christoph Paasch <christoph.paasch@gmail.com>" >> $ctrl
 echo "Description: A meta-package for linux-mptcp" >> $ctrl
