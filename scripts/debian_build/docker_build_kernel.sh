a57be32a75bb6b8d688f4c1fbfe5ccb5a83a32a3
Matthieu Baerts <matthieu.baerts@tessares.net>
2018-10-24 19:20:38 +0200

    kernel:fedora: build using Docker
    Add config and script to build the image and use it.
    Scripts are the same as the ones for debian: symlinks are used.
    
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

--- scripts/debian_build/docker_build_kernel.sh
+++ scripts/debian_build/docker_build_kernel.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -ex
 
+DISTRIB="$(basename "$(dirname "${PWD}/docker_build_kernel.sh")" | cut -d_ -f1)"
+
 # Create docker
-./create_docker_kernel.sh "debian"
+./create_docker_kernel.sh "${DISTRIB}"
 
 # Run docker
 MY_ROOT_DIR=${1:-"$(grep ${SUDO_USER:-"${USERNAME}"} /etc/passwd | cut -d: -f6)"}
@@ -11,6 +13,12 @@ EXTRA_ARGS=("-v" "${MY_ROOT_DIR}:${MY_ROOT_DIR}:rw")
 # to make sure they are both available in the docker
 if [[ "${PWD}" != "${MY_ROOT_DIR}"* ]]; then
 	EXTRA_ARGS+=("-v" "${PWD}:${PWD}:ro")
+fi
+
+# by default, RPM are produced in ${HOME}/rpmbuild but user is root in docker
+if [ "${DISTRIB}" == "fedora" ]; then
+	EXTRA_ARGS+=("-e" "RPMOPTS=--define '_topdir ${MY_ROOT_DIR}/rpm'")
+	rm -rf "${MY_ROOT_DIR}/rpm"
 fi
 
 add_args() {
@@ -24,4 +32,4 @@ add_args MY_EMAIL
 add_args MY_ROOT_DIR
 add_args MY_CONFIG
 
-docker run -it --rm "${EXTRA_ARGS[@]}" "debian-kernel" bash -ex "${PWD}/create_kernel.sh"
+docker run -it --rm "${EXTRA_ARGS[@]}" "${DISTRIB}-kernel" bash -ex "${PWD}/create_kernel.sh"
