0e2551dba81258a0cfc376c624fc1cf30daa0d8d
Matthieu Baerts <matthieu.baerts@tessares.net>
2018-10-24 18:35:17 +0200

    kernel:fedora: ability to manage the config
    Copied at the beginning, saved at the end.
    
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

--- scripts/fedora_build/create_kernel.sh
+++ scripts/fedora_build/create_kernel.sh
@@ -4,10 +4,24 @@
 
 # These variables can be modified
 ROOT_DIR=${MY_ROOT_DIR:-"${HOME}"}
+CONFIG=${MY_CONFIG:-"n"}
+
+# Unmodifiabled variables
+INIT_DIR="${PWD}"
+SCRIPT_DIR="${INIT_DIR}/$(dirname "${0}")"
 
 cd "${ROOT_DIR}/mptcp"
 
+KVERS=$(make kernelversion)
+KVERS_MAJ=$(echo "${KVERS}" | cut -d. -f1-2)
+CONFIG_KVERS="config-${KVERS_MAJ}"
+CONFIG_PATH="${SCRIPT_DIR}/${CONFIG_KVERS}"
+
+[ "${CONFIG}" = "y" ] && cp -v "${CONFIG_PATH}" .config
+
 make -j 8 rpm-pkg LOCALVERSION=.mptcp
+
+[ "${CONFIG}" = "y" ] && cp -v .config "${CONFIG_PATH}"
 
 # Install with 'dnf install kernel-4.1.34.mptcp' - may need to remove a kernel
 
