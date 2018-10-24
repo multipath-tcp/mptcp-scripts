95579f127a02546befa468fcacc67f9ca44557d9
Matthieu Baerts <matthieu.baerts@tessares.net>
2018-10-24 18:31:39 +0200

    kernel:fedora: ability to change the root dir
    Instead of using ${HOME} to find the MPTCP repo.
    
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

--- scripts/fedora_build/create_kernel.sh
+++ scripts/fedora_build/create_kernel.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+# Note: mptcp repo needs to be in "${MY_ROOT_DIR}/mptcp" at the correct ref.
+# If MY_ROOT_DIR is not set, it is "${HOME}"
 
-cd $HOME
+# These variables can be modified
+ROOT_DIR=${MY_ROOT_DIR:-"${HOME}"}
 
-cd $HOME/mptcp
+cd "${ROOT_DIR}/mptcp"
 
 make -j 8 rpm-pkg LOCALVERSION=.mptcp
 
