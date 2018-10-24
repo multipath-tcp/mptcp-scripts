b17c4e5b6d293e9ac70e0eccfb301ea5b7d9070c
Matthieu Baerts <matthieu.baerts@tessares.net>
2018-10-24 18:24:10 +0200

    kernel:fedora: remove debian refs
    Signed-off-by: Matthieu Baerts <matthieu.baerts@tessares.net>

--- scripts/fedora_build/create_kernel.sh
+++ scripts/fedora_build/create_kernel.sh
@@ -1,36 +1,10 @@
 #!/bin/bash
 
 cd $HOME
-rm -f *.deb
 
 cd $HOME/mptcp
 
-DATE=`date "+%Y%m%d%H%M%S"`
-KVERS=`make kernelversion`
-make -j 8 rpm-pkg DEBEMAIL='christoph.paasch@gmail.com' DEBFULLNAME='Christoph Paasch' LOCALVERSION=.mptcp KDEB_PKGVERSION=${DATE}
+make -j 8 rpm-pkg LOCALVERSION=.mptcp
 
 # Install with 'dnf install kernel-4.1.34.mptcp' - may need to remove a kernel
-
-## Create meta-package
-#rm -Rf linux-mptcp
-#
-#mkdir linux-mptcp
-#mkdir linux-mptcp/DEBIAN
-#chmod -R a-s linux-mptcp
-#ctrl="linux-mptcp/DEBIAN/control"
-#touch $ctrl
-#
-#echo "Package: linux-mptcp" >> $ctrl
-#echo "Version: ${DATE}" >> $ctrl
-#echo "Section: main" >> $ctrl
-#echo "Priority: optional" >> $ctrl
-#echo "Architecture: all" >> $ctrl
-#echo "Depends: linux-headers-${KVERS}, linux-image-${KVERS}" >> $ctrl
-#echo "Installed-Size:" >> $ctrl
-#echo "Maintainer: Christoph Paasch <christoph.paasch@gmail.com>" >> $ctrl
-#echo "Description: A meta-package for linux-mptcp" >> $ctrl
-#
-#dpkg --build linux-mptcp
-#
-#mv linux-mptcp.deb linux-mptcp_${DATE}_all.deb
 
