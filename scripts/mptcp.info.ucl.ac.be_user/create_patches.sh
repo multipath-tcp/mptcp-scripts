d0e9a6428d656fa8a7f4a505ded26cdd5847fa02
Christoph Paasch <christoph.paasch@gmail.com>
2022-01-07 20:05:11 +0100

    Fix create_patches

--- scripts/mptcp.info.ucl.ac.be_user/create_patches.sh
+++ scripts/mptcp.info.ucl.ac.be_user/create_patches.sh
@@ -7,25 +7,20 @@ exec > $logfile 2>&1
 trap "cat $logfile | uuencode $logfile | mailx -s \"$file failed\" christoph.paasch@gmail.com ; exit 1" ERR
 
 function gen_patch {
-	VERSION=`git log --oneline --merges | grep -m 1 "Merge tag" | cut -d "'" -f2 | cut -d "'" -f1`
+	VERSION=`git log --oneline --merges origin/$1 | grep -m 1 "Merge tag" | cut -d "'" -f2 | cut -d "'" -f1`
 
 	rm /home/ftp/mptcp-patches/mptcp-${VERSION}-*.patch
-	git diff $VERSION > /home/ftp/mptcp-patches/mptcp-${VERSION}-`git rev-parse --short HEAD`.patch
-}
-
-function repo {
-	git checkout $1
-	git pull
-	gen_patch
+	git diff ${VERSION}..origin/$1 > /home/ftp/mptcp-patches/mptcp-${VERSION}-`git rev-parse --short origin/${1}`.patch
 }
 
 cd $HOME/mtcp/
 
-git fetch stable
+git fetch --tags --all
 
-repo 'mptcp_trunk'
+gen_patch 'mptcp_trunk'
 for i in $(seq 91 96); do
-	repo "mptcp_v0.${i}"
+	gen_patch "mptcp_v0.${i}"
 done
 
 git checkout mptcp_trunk
+git pull
